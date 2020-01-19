f480d40bb007826099d58908de386908fadd74bc
James Kim <james.kim.nz92@gmail.com>
2020-01-19 22:36:24 +1300

    msg

--- plugin.php
+++ plugin.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Plugin Name: west-coast-block-new — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: west-coast-block-new — is a Gutenberg plugin created via create-guten-block.
- * Version: 0.0.01
+ * Plugin Name: West Coast Penguin Trust - blocks 
+ * Description: Custom blocks. 
+ * Version: 0.1.0
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
  * @package CGB
